c12c10467b48b2ba8f1bbaeb3e88a3d92ba506f8
mohammedsemo264 <mohammedsemo264@gmail.com>
2025-03-29 18:42:16 +0000

    Add 18.ts

---
+++ src/18.ts
@@ -0,0 +1,11 @@
+class ExampleClass {
+    constructor() {
+        this.data = {
+            key1: 'value1',
+            key2: 'value2'
+        };
+    }
+}
+
+const exampleClass = new ExampleClass();
+console.log(exampleClass);
