5d30f6789e25acda0a62fddb8a1b3764b0a09680
mohammedsemo264 <mohammedsemo264@gmail.com>
2025-03-30 06:24:58 +0000

    Add 19.ts

---
+++ src/19.ts
@@ -0,0 +1,13 @@
+function square(x: number): number {
+  return x * x;
+}
+
+// Example usage
+let mySquare = square(5);
+console.log(mySquare); // Outputs 25
+
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+addNumbers(3, 4); // Outputs 7
